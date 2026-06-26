4ffa8ff7cbd1bcb65701da99f3fa0dbaedf5ee92
Quent-ger <qgerodrigues@outlook.fr>
2026-06-26 11:52:26 +0200

    S2010 - S2011 partie 2

--- src/main/java/fr/eni/ludotheque/dto/AdresseDTO.java
+++ src/main/java/fr/eni/ludotheque/dto/AdresseDTO.java
@@ -1,4 +1,15 @@
 package fr.eni.ludotheque.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdresseDTO {
+
+    private String rue;
+    private String ville;
+    private String codePostal;
 }
